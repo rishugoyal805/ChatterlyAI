52b977c46d90be367d4922a47f420941dcc994a5
rishugoyal805 <rishugoyal16800@gmail.com>
2025-06-07 21:51:57 +0530

    update create new chat  route

--- app/api/create-new-chat/route.js
+++ app/api/create-new-chat/route.js
@@ -3,7 +3,7 @@ import { connectToDatabase } from '@/lib/mongodb';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import { ObjectId } from 'mongodb';
-
+ 
 export async function POST() {
   try {
     const session = await getServerSession(authOptions);
